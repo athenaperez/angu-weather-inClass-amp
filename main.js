3614d44d4830a8b52dd011e130e312da498701d6
Athena Perez <athenaperez@me.com>
2017-01-19 13:20:59 -0600

    added partials and factory and controllers

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 angular
-	.module('anguWeathar' ['ngRoute'])
+	.module('anguWeathar', ['ngRoute'])
 //creates a module(name, dependencies)
 	.config(($routeProvider) => {
 	//a callbackfunction(a function inside)
@@ -8,12 +8,55 @@ angular
 		//inject route provider into the config
 				controller: 'RootCtrl',
 				templateUrl: '/partials/root.html',
-
-
 		})
+			.when('/weather/:zipcode', {
+				controller: 'WeatherCtrl',
+				templateUrl: '/partials/weather.html',
+			})
 	})
 
-	.controller('RootCtrl', function () {
+
+// _____________________________
+	.controller('RootCtrl', function ($scope, $location) {
 	//don't use arrow functions for controllers
 		console.log(`I am a RootCtrl`)
+		$scope.gotoWeather = () => {
+			//change the url
+			// vanilla js version --> location.href = `/#!/weather/${$scope.zip}`
+			$location.url(`/weather/${$scope.zip}`)
+		}
 	})
+
+	.controller('WeatherCtrl', function ($scope, weatherFactory, $routeParams) {
+	//don't use arrow functions for controllers
+		console.log(`I am a WeatherCtrl`)
+
+
+// _____________________________
+
+		weatherFactory
+			.getWeather($routeParams.zipcode)
+			.then((weather) => {
+				$scope.temperature = weather.temp
+				$scope.city = weather.city
+			})
+		})
+
+	.factory('weatherFactory', ($http) => {
+		return {
+			getWeather(zipcode) {
+				return $http
+				.get(`http://api.wunderground.com/api/9f01283203df51cf/conditions/q/${zipcode}.json`)
+				.then((response) => ({
+					temp: response.data.current_observation.temp_f,
+					city: response.data.current_observation.display_location.full,
+				})
+			)
+		},
+	}
+})
+
+
+
+
+// _____________________________
